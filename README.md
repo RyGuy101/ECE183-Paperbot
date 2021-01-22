5e55377fda3e853aae45516cec7ce9e0d5ccf06a
Ryan Nemiroff <ryguyn@gmail.com>
2021-01-22 12:36:47 -0800

    It's animated!

--- README.md
+++ README.md
@@ -1,25 +1,39 @@
-#Segway and Paperbot
-##Description
+# Segway and Paperbot Simulation
+## Description
+---
+The objective of this lab was to create a mathematical formulation and simulate two-wheeled robots, more specifically a Segway and a Paperbot, each with a wheel diameter *d* and width *w*. The computational simulation was made to simulate either Segway or Paperbot in a rectangular environment with 4 walls and boundaries specified by *xW*, *xE*, *yN*, *yS*, representing the west, east, north, and south wall limits respecively.
+
 **Mathematical Formulations**
-[Math Formulation]() //Include link to math formulation here
+* [Math Formulation]() //Include link to math formulation here
 
-For Joint Lab 1, we created mathematical formulations of two-wheeled robots 
-according to the specifications [here](https://ccle.ucla.edu/pluginfile.php/4059849/mod_resource/content/0/ME_ECE_Joint_Lab_1.pdf). Using these mathematical models, we then simulated the two-wheeled robot
 
-##Usage 
+## Usage 
+---
 The simulation is initialized with the user-specified values:
-* West xW, East xE, North yN, South yS coordinate values for the fixed x or y 
+* West *xW*, East *xE*, North *yN*, South *yS* coordinate values for the fixed x or y 
 positions  of the walls, respectively
-* Segway Dimenstions d = 502mm, w = 530mm, or Paperbot Dimensions d = 50mm, w = 90mm
-* Dr, the right distance sensor offset
-* Df, the front distance sensor offset  
-* H, the actuator/state transition covariance
-* V, the sensor/output covariance
-* dt, the sample period  
+* Segway Dimenstions *d* = 502mm, *w* = 530mm; or Paperbot Dimensions *d* = 50mm, *w* = 90mm
+* *Dr*, the right distance sensor offset
+* *Df*, the front distance sensor offset  
+* *H*, the actuator/state transition covariance
+* *V*, the sensor/output covariance
+* *dt*, the sample period  
 
 ```python
-<insert python code here to run simulation>
+# Insert code on how to use simulation here
 ```
+
+## Hardware
+---
+* Segway
+  * Continuous Rotation Servo: Unspecified
+  * Laser Range Sensor: [Lidar Lite v3](https://cdn.sparkfun.com/assets/f/e/6/3/7/PM-14032.pdf)
+  * IMU: [3DM-GX5-10, LORD MicroStrain](https://www.microstrain.com/sites/default/files/3dm-gx5-25_datasheet_8400-0093_rev_o.pdf)
+* Paperbot
+  * Continuous Rotation Servo: [FS90R](https://cdn-shop.adafruit.com/product-files/2442/FS90R-V2.0_specs.pdf)
+  * Laser Range Sensor: [GYVL53L0X](https://www.st.com/resource/en/datasheet/vl53l0x.pdf)
+  * IMU: [MPU9250](https://invensense.tdk.com/products/motion-tracking/9-axis/mpu-9250/?fbclid=IwAR3oB_TRBq6vESyrCZcruXoCS__1Q0_s-4oi9rOmKmrxwR31XUUP7iYXZNA)
+
 
 
 
--- paperbot.py
+++ paperbot.py
@@ -7,9 +7,13 @@
 
 import numpy as np
 import math
+import time
+import keyboard
+import pygame, sys
+from pygame.locals import *
 
 class Bot:
-    def __init__(self, xW, xE, yN, yS, d=50, w=90, Dr=0, Df=0, H=np.zeros([3,3]), V=np.zeros([3,3]), dt=0.1, init_state=np.array([-200.0, -200.0, 0.0])):
+    def __init__(self, xW=-500, xE=0, yN=0, yS=-500, d=50, w=90, Dr=0, Df=0, max_wheel_speed=2*math.pi*130, dt=0.1, init_state=np.array([-250.0, -250.0, 0.0])):
         self.xW = xW # western wall position
         self.xE = xE # eastern wall position
         self.yN = yN # northern wall position
@@ -18,37 +22,65 @@ class Bot:
         self.w = w # robot width (distance between wheels)
         self.Dr = Dr # right distance sensor offset
         self.Df = Df # front distance sensor offset
+        self.max_wheel_speed = max_wheel_speed
         # Above distance sensor offsets unknown for now
-        self.H = H # actuator/state transition covariance
-        self.V = V # sensor/output covariance
         self.dt = dt # sample period
         self.state = init_state
 
     def iterate(self, u):
         next_state_mean, next_state_cov = self.next_state_mean_and_cov(u)
         output_mean, output_cov = self.output_mean_and_cov(u)
-        self.state = next_state_mean + self.generate_noise(next_state_cov)
-        return output_mean + self.generate_noise(output_cov)
+        self.state = self.add_noise(next_state_mean, next_state_cov)
+
+        # constrain theta to [0, 2pi)
+        while (self.state[2] < 0):
+            self.state[2] += 2*math.pi
+        while (self.state[2] >= 2*math.pi):
+            self.state[2] -= 2*math.pi
+
+        return self.add_noise(output_mean, output_cov)
 
     def next_state_mean_and_cov(self, u):
-        next_state_mean = np.zeros(3)
-        next_state_mean[2] = self.state[2] + self.robot_angular_velocity(u)*self.dt
         robot_center_velocity = self.robot_center_velocity(u)
-        next_state_mean[0] = self.state[0] + robot_center_velocity*math.cos(self.state[2])*self.dt
-        next_state_mean[1] = self.state[1] + robot_center_velocity*math.sin(self.state[2])*self.dt
-        return next_state_mean, self.H
+        dx = robot_center_velocity*math.cos(self.state[2])*self.dt
+        dy = robot_center_velocity*math.sin(self.state[2])*self.dt
+        dtheta = self.robot_angular_velocity(u)*self.dt
+
+        next_state_mean = np.zeros(3)
+        next_state_mean[0] = self.state[0] + dx
+        next_state_mean[1] = self.state[1] + dy
+        next_state_mean[2] = self.state[2] + dtheta
+
+        return next_state_mean, np.diag([(dx**2)*(0.03**2), (dy**2)*(0.03**2), (dtheta**2)*(0.03**2)])
 
     def output_mean_and_cov(self, u):
-        return np.array([0.0,0,0,0,0]), self.V
+        output_mean = np.zeros(5)
+        output_mean[0] = self.expected_right_sensor_measurement()
+        output_mean[1] = self.expected_front_sensor_measurement()
+        output_mean[2] = self.robot_angular_velocity(u)
+        output_mean[3:5] = self.expected_magnetometer_measurement()
+
+        return \
+            output_mean, \
+            np.diag([ # TODO sensor noise
+                0,
+                0,
+                0,
+                0,
+                0
+            ])
 
     def pwm_to_wheel_angular_velocity(self, pwm):
-        return 0 # return array with left and right angular velocity
+        # if abs(pwm) > 0.05625:
+            return (self.max_wheel_speed*pwm) * (1 - 0.4*pwm)
+        # else:
+        #     return 0
 
     def robot_angular_velocity(self, u):
-        return (self.d/2)*(self.pwm_to_wheel_angular_velocity(u[1])[1] - self.pwm_to_wheel_angular_velocity(u[0])[0])/self.w
+        return (self.d/2)*(self.pwm_to_wheel_angular_velocity(u[1]) - self.pwm_to_wheel_angular_velocity(u[0]))/self.w
 
     def robot_center_velocity(self, u):
-        return ((self.d/2)*self.pwm_to_wheel_angular_velocity(u[0])[0] + (self.d/2)*self.pwm_to_wheel_angular_velocity(u[1])[1])/2
+        return ((self.d/2)*self.pwm_to_wheel_angular_velocity(u[0]) + (self.d/2)*self.pwm_to_wheel_angular_velocity(u[1]))/2
 
     def expected_right_sensor_measurement(self):
         # Following y - y_t = (tan theta)(x - x_t)
@@ -58,16 +90,16 @@ class Bot:
         if theta < 0:
             raise ValueError("ERROR: In-plane velocity angle incorrect")
         elif theta < math.pi/2:
-            xR1 = self.xE 
-            yR2 = self.yN
+            xR1 = self.xE
+            yR2 = self.yS
         elif theta < math.pi:
-            xR1 = self.xW
+            xR1 = self.xE 
             yR2 = self.yN
         elif theta < (3*math.pi/2):
             xR1 = self.xW
-            yR2 = self.yS
+            yR2 = self.yN
         elif theta < (2*math.pi):
-            xR1 = self.xE
+            xR1 = self.xW
             yR2 = self.yS
         else:
             raise ValueError("ERROR: In-plane velocity angle incorrect")
@@ -92,17 +124,17 @@ class Bot:
         if theta < 0:
             raise ValueError("ERROR: In-plane velocity angle incorrect")
         elif theta < math.pi/2:
-            xF1 = self.xW 
+            xF1 = self.xE
             yF2 = self.yN
         elif theta < math.pi:
-            xF1 = self.xW
-            yF2 = self.yS
+            xF1 = self.xW 
+            yF2 = self.yN
         elif theta < (3*math.pi/2):
-            xF1 = self.xE
+            xF1 = self.xW
             yF2 = self.yS
         elif theta < (2*math.pi):
             xF1 = self.xE
-            yF2 = self.yN
+            yF2 = self.yS
         else:
             raise ValueError("ERROR: In-plane velocity angle incorrect")
         
@@ -119,8 +151,64 @@ class Bot:
         return min(ew_wall_dist, nw_wall_dist) - self.Df
 
     def expected_magnetometer_measurement(self):
-        return 0
+        return np.zeros(2)
 
-    def generate_noise(self, cov):
-        return np.random.multivariate_normal(np.zeros(cov.shape[0]), cov)
+    def add_noise(self, mean, cov):
+        return np.random.multivariate_normal(mean, cov)
 
+
+def rotate_vector(v, radians):
+    x, y = v
+    xx = x * math.cos(radians) + y * math.sin(radians)
+    yy = -x * math.sin(radians) + y * math.cos(radians)
+    return np.array([xx, yy])
+
+def main():
+    paperbot = Bot()
+
+    pygame.init()
+    DISPLAY=pygame.display.set_mode((-paperbot.xW, -paperbot.yS), 0, 32)
+    WHITE=(255,255,255)
+    BLUE=(0,0,255)
+    
+    while True:
+        u = np.zeros(2)
+        if keyboard.is_pressed('up arrow'):
+            u[0] = 0.01
+            u[1] = 0.01
+        elif keyboard.is_pressed('down arrow'):
+            u[0] = -0.01
+            u[1] = -0.01
+        elif keyboard.is_pressed('left arrow'):
+            u[0] = -0.01
+            u[1] = 0.01
+        elif keyboard.is_pressed('right arrow'):
+            u[0] = 0.01
+            u[1] = -0.01
+        
+        output = paperbot.iterate(u)
+
+        DISPLAY.fill(WHITE)
+
+        rect_center = np.array([-paperbot.xW + paperbot.state[0], -paperbot.state[1]])
+
+        rect_points = [
+            np.array([-paperbot.d/2, -paperbot.w/2]),
+            np.array([-paperbot.d/2, +paperbot.w/2]),
+            np.array([+paperbot.d/2, +paperbot.w/2]),
+            np.array([+paperbot.d/2, -paperbot.w/2])
+        ]
+
+        for i in range(len(rect_points)):
+            rect_points[i] = rect_center + rotate_vector(rect_points[i], paperbot.state[2])
+
+        pygame.draw.polygon(DISPLAY, BLUE, rect_points)
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+        time.sleep(paperbot.dt)
+
+main()
